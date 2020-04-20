a7ec2bb7ef88201b25c7e9bbd30b6084fb6b5cc8
hesperiusproject <53948201+hesperiusproject@users.noreply.github.com>
2020-04-20 10:47:27 +0200

    WIP

---
+++ MVN_MC_AD_API/src/it/taa/gallmetzer/maa/MAA.java
@@ -0,0 +1,216 @@
+package it.taa.gallmetzer.maa;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import it.taa.gallmetzer.maa.callbacks.CheckCallback;
+import it.taa.gallmetzer.maa.callbacks.CreateCallback;
+import it.taa.gallmetzer.maa.utils.Challenge;
+import it.taa.gallmetzer.maa.utils.RequestHandler;
+import ly.adf.AdflyApiWrapper;
+
+public class MAA<T extends JavaPlugin> {
+
+	private static HashMap<Player, Challenge> currentChallenges = new HashMap<Player, Challenge>();
+	public static String domain;
+	public static String public_key;
+	public static String secret_key;
+	public static int user_id;
+	private T plugin;
+	
+	@SuppressWarnings("rawtypes")
+	public static MAA maa;
+	
+	public MAA(T plugin, String domain_, String public_key_, String secret_key_, int user_id_) {
+		
+		this.plugin = plugin;
+		domain = domain_;
+		public_key = public_key_;
+		secret_key = secret_key_;
+		user_id = user_id_;
+		maa = this;
+	}
+	
+	public static MAA get() {
+		return maa;
+	}
+	
+	
+	public  void addChallenge(final Player p, final CreateCallback b) {
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+			public void run() {
+				if (MAA.getChallenge(p) != null) {
+					b.asyncAddPlayer("Error 3");
+					return;
+				}
+
+				String unshort = RequestHandler.addPlayer(p);
+				if (unshort != null) {
+					String link = getApiRes("http://" + domain + "/verify/" + unshort);
+					if (link != null) {
+						currentChallenges.put(p, new Challenge(getID(link)));
+						b.asyncAddPlayer(justUrl(link));
+					} else {
+						b.asyncAddPlayer("Error 2");
+					}
+				} else {
+					b.asyncAddPlayer("Error 1");
+				}
+			}
+
+		});
+	}
+
+	@Deprecated
+	protected void deleteIfExists(final Player p) {
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+			public void run() {
+				if (MAA.addChallenge(p) != null) {
+					AdflyApiWrapper api = new AdflyApiWrapper();
+					api.deleteUrl(MAA.getChallenge(p).getID());
+					return;
+				}
+			}
+		});
+	}
+
+	protected static String addChallenge(Player player) {
+		if (MAA.getChallenge(player) != null) {
+			return "Error 3";
+		}
+		String unshort = RequestHandler.addPlayer(player);
+		if (unshort != null) {
+			String link = getApiRes("http://" + domain + "/verify/" + unshort);
+			if (link != null) {
+				currentChallenges.put(player, new Challenge(getID(link)));
+				return justUrl(link);
+			} else {
+				return "Error 2";
+			}
+		} else {
+			return "Error 1";
+		}
+
+	}
+
+	public boolean checkPlayer(Player p) {
+		boolean complete = RequestHandler.checkPlayer(p);
+		if (complete) {
+			MAA.getChallenge(p).setBool(true);
+			RequestHandler.deleteLink(MAA.getChallenge(p).getID(), this.plugin);
+			MAA.hasCompleted(p);
+
+		} else if (!complete && MAA.getChallenge(p) == null) {
+			complete = true;
+		}
+		return complete;
+	}
+
+	public void checkPlayer(final Player p, final CheckCallback c) {
+		final T plug = this.plugin;
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+
+			public void run() {
+				boolean complete = RequestHandler.checkPlayer(p);
+				if (complete) {
+					MAA.getChallenge(p).setBool(true);
+					RequestHandler.deleteLink(MAA.getChallenge(p).getID(), plug);
+					MAA.hasCompleted(p);
+
+				} else if (!complete && MAA.getChallenge(p) == null) {
+					complete = true;
+				}
+
+				c.asyncCheckPlayer(complete);
+			}
+
+		});
+
+	}
+
+	public static boolean hasCompleted(Player p) {
+		try {
+			if (!currentChallenges.get(p).hasPassed()) {
+				return false;
+			} else if (currentChallenges.get(p).hasPassed()) {
+				currentChallenges.remove(p);
+				return true;
+			}
+		} catch (NullPointerException e) {
+		}
+		return true;
+
+	}
+
+	public static void clean() {
+		AdflyApiWrapper api = new AdflyApiWrapper();
+		for (Player p : getCurrentChallenges().keySet()) {
+			api.deleteUrl(MAA.getChallenge(p).getID());
+		}
+		return;
+	}
+
+	public static Challenge getChallenge(Player p) {
+		return currentChallenges.get(p);
+	}
+
+	private static String getApiRes(String url) {
+		AdflyApiWrapper api = new AdflyApiWrapper();
+		String a = api.shorten(url);
+		JsonParser prs = new JsonParser();
+		JsonObject obj = (JsonObject) prs.parse(a);
+		return obj.getAsJsonArray("data").get(0).getAsJsonObject().getAsJsonPrimitive("short_url").toString()
+				+ obj.getAsJsonArray("data").get(0).getAsJsonObject().getAsJsonPrimitive("id").toString();
+	}
+
+	private static String justUrl(String dirt) {
+		String newa = dirt.substring(1);
+		String ret = "";
+		for (int i = 0; i < newa.length(); i++) {
+			if (newa.charAt(i) != '"') {
+				ret = ret + newa.charAt(i);
+			} else {
+				return ret;
+			}
+		}
+		return dirt;
+	}
+
+	private static long getID(String ds) {
+		String dirt = ds.substring(1);
+		for (int i = 0; i < dirt.length(); i++) {
+			if (dirt.charAt(i) != '"') {
+			} else {
+				String finl = dirt.substring(i + 1);
+
+				return Long.parseLong(finl.substring(1, finl.length() - 1));
+			}
+		}
+		return 0;
+	}
+
+	// SETTERS & GETTERS
+
+	public static String getDomain() {
+		return domain;
+	}
+
+	public static HashMap<Player, Challenge> getCurrentChallenges() {
+		return currentChallenges;
+	}
+
+	public static boolean activeChallenges() {
+		return currentChallenges.isEmpty();
+	}
+
+	public static void setDomain(String s) {
+		domain = s;
+	}
+
+}
---
+++ MVN_MC_AD_API/src/it/taa/gallmetzer/maa/utils/RequestHandler.java
@@ -0,0 +1,167 @@
+package it.taa.gallmetzer.maa.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import it.taa.gallmetzer.maa.MAA;
+import ly.adf.AdflyApiWrapper;
+
+public class RequestHandler {
+
+	public static String addPlayer(Player p) {
+		try {
+			String postUrl = "http://" + MAA.getDomain() + "/add";// put in your url
+			Gson gson = new Gson();
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			HttpPost post = new HttpPost(postUrl);
+			String url = generateUrl(p);
+			StringEntity postingString = new StringEntity(gson.toJson(new Request(p, url)));
+
+			post.setEntity(postingString);
+			post.setHeader("Content-type", "application/json");
+
+			httpClient.execute(post);
+			return url;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+
+	}
+
+	public static boolean checkPlayer(Player p) {
+		try {
+			String postUrl = "http://" + MAA.getDomain() + "/hasverified";
+			Gson gson = new Gson();
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			HttpPost post = new HttpPost(postUrl);
+			StringEntity postingString = new StringEntity(gson.toJson(new Request(p, "NONE")));
+
+			post.setEntity(postingString);
+			post.setHeader("Content-type", "application/json");
+
+			HttpResponse response = httpClient.execute(post);
+			HttpEntity entity = response.getEntity();
+			String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+			JsonParser ps = new JsonParser();
+			JsonObject obj = (JsonObject) ps.parse(json);
+			boolean b = obj.getAsJsonPrimitive("completed").getAsBoolean();
+			return b;
+		} catch (Exception e) {
+			System.out.println(e);
+			return true;
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public static <T extends JavaPlugin> void start(final T plugin) {
+
+		BukkitScheduler scheduler = plugin.getServer().getScheduler();
+		scheduler.scheduleAsyncRepeatingTask(plugin, new Runnable() {
+			public void run() {
+				if (ChallengeCompletionEvent.getHandlerList().getRegisteredListeners().length == 0
+						|| MAA.activeChallenges()) {
+					return;
+				} else if (ChallengeCompletionEvent.getHandlerList().getRegisteredListeners().length > 0
+						&& !MAA.activeChallenges()) {
+
+					try {
+						String postUrl = "http://" + MAA.getDomain() + "/getcomplete";
+						Gson gson = new Gson();
+						HttpClient httpClient = HttpClientBuilder.create().build();
+						HttpPost post = new HttpPost(postUrl);
+						StringEntity postingString = new StringEntity(gson.toJson(new GetCompleted()));
+
+						post.setEntity(postingString);
+						post.setHeader("Content-type", "application/json");
+
+						HttpResponse response = httpClient.execute(post);
+						HttpEntity entity = response.getEntity();
+						String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+
+						JsonParser ps = new JsonParser();
+
+						if (ps.parse(json) instanceof JsonNull) {
+							return;
+						}
+						final String toGo = json;
+						Bukkit.getServer().getScheduler().runTask(plugin, new Runnable() {
+
+							public void run() {
+								JsonParser s = new JsonParser();
+								JsonObject obj = (JsonObject) s.parse(toGo);
+								for (Object o : obj.getAsJsonArray("active")) {
+									ChallengeCompletionEvent event = new ChallengeCompletionEvent(
+											Bukkit.getPlayer(UUID
+													.fromString(o.toString().substring(1, o.toString().length() - 1))),
+											plugin);
+									Bukkit.getPluginManager().callEvent(event);
+
+								}
+							}
+						});
+
+						return;
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+				}
+			}
+		}, 20L, 10 * 20L);
+	}
+
+	public static <T extends JavaPlugin> void deleteLink(final long id, T plugin) {
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+			public void run() {
+				AdflyApiWrapper api = new AdflyApiWrapper();
+				api.deleteUrl(id);
+			}
+		});
+
+	}
+
+	private static String generateUrl(Player p) {
+
+		String random = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+		String randomS = p.getUniqueId().toString() + "-";
+		for (int i = 0; i < 4; i++) {
+			randomS = randomS + random.charAt((int) (Math.random() * random.length()));
+		}
+
+		String s = p.getUniqueId().toString() + "-" + randomS;
+		return s;
+	}
+
+}
+
+class Request {
+	String UUID;
+	String link;
+
+	public Request(Player p, String link) {
+		this.UUID = p.getUniqueId().toString();
+		this.link = link;
+	}
+
+}
+
+class GetCompleted {
+	String info = Bukkit.getName() + ": Requesting completed challenges";
+}
